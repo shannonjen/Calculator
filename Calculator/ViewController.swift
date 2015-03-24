6d3e4b184d6ee73a0870117a6481a294accec2ad
Jennifer Shannon <jen.shannon@gmail.com>
2015-03-24 08:42:59 -0400

    Stanford iOS8 w/Swift Lesson 1

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jen on 3/24/15.
+//  Copyright (c) 2015 Jen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTypingANumber: Bool = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTypingANumber{
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingANumber = true
+        }
+    }
+    
+}
+
